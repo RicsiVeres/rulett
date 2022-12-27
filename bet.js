a77a4d5cef1c43e994925ca391a3ef4b26f7c4b4
RicsiVeres <120040566+RicsiVeres@users.noreply.github.com>
2022-12-27 21:20:18 +0200

    Add files via upload

--- bet.js
+++ bet.js
@@ -1,6 +1,53 @@
 let x = document.getElementsByTagName("button");
+let x5 = document.querySelector("#zseton");
+let kijeloltbet = 25;
+let zseton = 5000;
+function penz() {
+    x5.innerHTML = zseton;
+}
 
-let parosak = [];
+let d = {
+    0:[],
+    1:[],
+    2:[],
+    3:[],
+    4:[],
+    5:[],
+    6:[],
+    7:[],
+    8:[],
+    9:[],
+    10:[],
+    11:[],
+    12:[],
+    13:[],
+    14:[],
+    15:[],
+    16:[],
+    17:[],
+    18:[],
+    19:[],
+    20:[],
+    21:[],
+    22:[],
+    23:[],
+    24:[],
+    25:[],
+    26:[],
+    27:[],
+    28:[],
+    29:[],
+    30:[],
+    31:[],
+    32:[],
+    33:[],
+    34:[],
+    35:[],
+    36:[],
+};
+function log() {
+    console.log(d);
+}
 function paros(item) {
     for (let i = 0; i < x.length; i++) {
         if (x[i].innerText % 2 === 0) {
@@ -85,4 +132,15 @@ function nulla(item) {
 
 function szam(item) {
     console.log(item.innerText);
+    zseton -= kijeloltbet;
+    d[item.innerText].push(kijeloltbet)
+    penz();
 }
+function kivalasztottBet(item) {
+    kijeloltbet = item.innerText;
+    console.log(kijeloltbet);
+}
+function vissza(item) {
+    log()
+}
+penz()
